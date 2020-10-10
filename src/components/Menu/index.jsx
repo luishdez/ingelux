1a532850faecb9ce931f750b423d5b8842b30652
Luis Hdez <luis.munoz.hdez@gmail.com>
2020-10-10 02:58:09 +0200

    Complete header

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,63 +1,47 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
-import Fade from 'react-reveal/Fade';
-import PortfolioContext from '../../context/context';
+import React from 'react';
+import Icon from '@mdi/react';
+import { mdiLinkedin } from '@mdi/js';
+import logo from '../../images/logos/right.svg';
 
 const Menu = () => {
-  const { hero } = useContext(PortfolioContext);
-
-  const [isDesktop, setIsDesktop] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    if (window.innerWidth > 769) {
-      setIsDesktop(true);
-      setIsMobile(false);
-    } else {
-      setIsMobile(true);
-      setIsDesktop(false);
-    }
-  }, []);
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">
-              Disabled
-            </a>
-          </li>
-        </ul>
+    <nav className="navbar fixed-top navbar-expand-md navbar-light">
+      <div className="container">
+        <a className="" href="/">
+          <img src={logo} alt="" style={{ height: 73 }} loading="lazy" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarText">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Proyectos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Contacto
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <Icon path={mdiLinkedin} title="User Profile" size={1.6} />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
