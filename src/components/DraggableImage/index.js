73cf7470c043d81b552da3ef2cf58553190216a8
Luis Hdez <luis.munoz.hdez@gmail.com>
2020-11-08 20:49:31 +0100

    Add image compare

--- src/components/DraggableImage/index.js
+++ src/components/DraggableImage/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-// import ReactCompareImage from 'react-image-compare';
-// import img1 from '../../images/projects/renderA.png';
-// import img2 from '../../images/projects/renderB.png';
+import ReactCompareImage from 'react-compare-image';
+import img1 from '../../images/projects/renderA.png';
+import img2 from '../../images/projects/renderB.png';
 
 const DraggableImage = () => {
   return (
-    <div className="container">
-      {/* <ReactCompareImage
-        leftImage={img1}
-        leftImageLabel="Before"
-        rightImage={img2}
-        rightImageLabel="After"
-      /> */}
-    </div>
+    <section id="project-hero" className="container" style={{ paddingTop: 100, paddingBottom: 40 }}>
+      <ReactCompareImage leftImage={img1} rightImage={img2} />
+    </section>
   );
 };
 
