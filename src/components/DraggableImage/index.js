c1471e1f12ebb4db3935ceb2aef735d887d868db
Luis Hdez <luis.munoz.hdez@gmail.com>
2020-12-08 19:18:53 +0100

    Improve draggable

--- src/components/DraggableImage/index.js
+++ src/components/DraggableImage/index.js
@@ -5,12 +5,16 @@ import img2 from '../../images/projects/renderB.png';
 
 const DraggableImage = () => {
   return (
-    <section id="project-hero" className="container" style={{ paddingTop: 100, paddingBottom: 40 }}>
+    <section
+      id="project-hero"
+      className="container draggable"
+      style={{ paddingTop: 100, paddingBottom: 40 }}
+    >
       <ReactCompareImage
-	sliderLineWidth="5"
-	sliderLineColor="#000"
-	leftImage={img1}
-	rightImage={img2}
+        sliderLineWidth="5"
+        sliderLineColor="#000"
+        leftImage={img1}
+        rightImage={img2}
       />
     </section>
   );
